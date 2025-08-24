91e9caa2d6597ec96df914ec3b9288813901e4bb
aleksey-pr <alelksey.pr.0711@outlook.com>
2025-08-24 06:22:09 -0700

    [JATP-21, JATP-25, JATP-29, JATP-41] Add Upload-Dataset-UI-API

--- wwwroot/js/form-basic-inputs.js
+++ wwwroot/js/form-basic-inputs.js
@@ -7,5 +7,5 @@
 (function () {
   // Indeterminate checkbox
   const checkbox = document.getElementById('defaultCheck2');
-  checkbox.indeterminate = true;
+  // checkbox.indeterminate = true;
 })();
